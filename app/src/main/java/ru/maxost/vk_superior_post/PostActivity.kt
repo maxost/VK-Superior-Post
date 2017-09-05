22dd8183ca051c2009eafa480f9f36d42d29c288
Maxim Ostrovidov <dustlooped@yandex.ru>
2017-09-06 09:52:16 +1000

    initial commit

---
+++ app/src/main/java/ru/maxost/vk_superior_post/PostActivity.kt
@@ -0,0 +1,12 @@
+package ru.maxost.vk_superior_post
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PostActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post)
+    }
+}

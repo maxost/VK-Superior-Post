90f01c40897f54720237d46ac49068f902ba84fc
Maxim Ostrovidov <dustlooped@yandex.ru>
2017-09-07 23:40:45 +1000

    wip commit

--- app/src/main/java/ru/maxost/vk_superior_post/Data/Services/Bitmap/ProdBitmapService.kt
+++ app/src/main/java/ru/maxost/vk_superior_post/Data/Services/Bitmap/ProdBitmapService.kt
@@ -1,10 +1,7 @@
 package ru.maxost.vk_superior_post.Data.Services.Bitmap
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Typeface
+import android.graphics.*
 import android.provider.FontRequest
 import android.provider.FontsContract
 import android.support.v4.content.ContextCompat
@@ -20,7 +17,10 @@ import android.view.Gravity
 import android.view.View
 import android.view.WindowManager
 import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.Switch
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import ru.maxost.switchlog.SwitchLog
 import ru.maxost.vk_superior_post.Utils.dp2px
 
@@ -34,9 +34,23 @@ class ProdBitmapService(private val context: Context): BitmapService {
     override fun createBitmap(post: Post): Single<Bitmap> = Single.fromCallable {
 
         //init
-        val bitmap = Bitmap.createBitmap(1080, 1080, Bitmap.Config.ARGB_8888)
+        val bitmap = Bitmap.createBitmap(1080, 1080, Bitmap.Config.RGB_565)
         val canvas = Canvas(bitmap)
-        canvas.drawColor(ContextCompat.getColor(context, R.color.white))
+        canvas.drawColor(ContextCompat.getColor(context, R.color.blue))
+
+        //stickers
+        val options = BitmapFactory.Options().apply { inJustDecodeBounds = true }
+        BitmapFactory.decodeResource(context.resources, R.drawable.sticker_fish_5, options)
+
+        val matrix = Matrix().apply {
+            setRotate(90f)
+            setTranslate(200f, 200f)
+        }
+        val sticker = BitmapFactory.decodeResource(context.resources, R.drawable.sticker_fish_5)
+        val stickerScaled = Bitmap.createScaledBitmap(sticker, 200, 200, true)
+
+        SwitchLog.scream("${stickerScaled.height} ${stickerScaled.width}")
+        canvas.drawBitmap(stickerScaled, matrix, null)
 
         val frameLayout = FrameLayout(context)
         frameLayout.isDrawingCacheEnabled = true
@@ -44,7 +58,7 @@ class ProdBitmapService(private val context: Context): BitmapService {
         //text
         val textView = TextView(context)
         textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24f)
-        textView.setTextColor(ContextCompat.getColor(context, R.color.black))
+        textView.setTextColor(ContextCompat.getColor(context, R.color.white))
         textView.text = post.text
         textView.typeface = ResourcesCompat.getFont(context, R.font.roboto_medium)
         textView.gravity = Gravity.CENTER

095241ad0764e17d6b4cfada64f28bd2af74d718
Maxim Ostrovidov <dustlooped@yandex.ru>
2017-09-19 22:01:50 +1000

    fixes

--- app/src/main/java/ru/maxost/vk_superior_post/UI/PostScreen/PostActivity.kt
+++ app/src/main/java/ru/maxost/vk_superior_post/UI/PostScreen/PostActivity.kt
@@ -51,7 +51,12 @@ class PostActivity : PostPresenter.View, StickerListDialogFragment.Listener, Key
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+
         setContentView(R.layout.activity_post)
         initListeners()
         initBackgroundsList()

01a3c0529501f6cf9b1ec460c87be7419e3d1da2
Maxim Ostrovidov <dustlooped@yandex.ru>
2017-09-21 00:45:50 +1000

    status bar color fix

--- app/src/main/java/ru/maxost/vk_superior_post/UI/LoginPresenter/LoginActivity.kt
+++ app/src/main/java/ru/maxost/vk_superior_post/UI/LoginPresenter/LoginActivity.kt
@@ -3,6 +3,7 @@ package ru.maxost.vk_superior_post.UI.LoginPresenter
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.vk.sdk.VKAccessToken
@@ -26,6 +27,7 @@ class LoginActivity : AppCompatActivity(), LoginPresenter.View {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
                     View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            window.statusBarColor = ContextCompat.getColor(this, R.color.coolBlue)
         }
 
         activity_login_button.setOnClickListener { presenter.onVkLoginClick() }
